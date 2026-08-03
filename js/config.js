d698f3412a7cd0b31b4e68a354d623b0b05c1f0a
Kford-Digitals <sahessstreet@gmail.com>
2026-08-03 15:38:57 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 const SUPABASE_URL = "https://ubsktyaaygnzkakwtkel.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_1vd3f0GnqdioHSkpd685lQ_kjwYzcr1";
-const PAYSTACK_PUBLIC_KEY = "pk_test_c4606a5f446d7a549e821ad4339bc78c66556e97";
+const PAYSTACK_PUBLIC_KEY = "pk_live_84428d1743617cc38a3ca1aa151a81e4cc57f3f7";
 
 // Initialize Supabase Client
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
